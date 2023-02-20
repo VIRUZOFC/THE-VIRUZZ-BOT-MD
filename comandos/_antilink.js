754443d21b5faa802ec70b51fb94bed916b85f26
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-20 18:32:31 -0300

    Update _antilink.js

--- comandos/_antilink.js
+++ comandos/_antilink.js
@@ -10,13 +10,14 @@ let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*𝙷𝙴𝚈!! 𝙴𝙻 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝙴𝚂𝚃𝙰 𝙰𝙲𝚃𝙸𝚅𝙾, 𝙿𝙴𝚁𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝙰𝙳𝙼𝙸𝙽 🤑, 𝚂𝙰𝙻𝚅𝙰𝙳𝙾/𝙰!*')
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*Has mandado un enlace al grupo pero eres admin, te has salvado 
+*')
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await conn.sendButton(m.chat, `*「 ANTI LINKS 」*\n*chao ${await this.getName(m.sender)} Mandaste un enlace y eso está prohibido aquí., Serás eliminado...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, wm3, db,  ['[DESACTIVAR ANTILINK]', '/disable antilink'], m)    
+await conn.sendButton(m.chat, `*「 ANTI LINKS 」*\n*chao ${await this.getName(m.sender)} Mandaste un enlace y eso está prohibido aquí., Serás eliminado...!!*${isBotAdmin ? '' : '\n\n*El bot no puede eliminar si no es admin del grupo*'}`, wm3, db,  ['[INFORMACIÓN DEL GRUPO]', '/infogrupo'], m)    
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
