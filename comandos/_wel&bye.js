06eddd27585fb4651bae4e4733a7b4c6f557beb1
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-21 09:23:16 -0300

    Update _wel&bye.js

--- comandos/_wel&bye.js
+++ comandos/_wel&bye.js
@@ -3,11 +3,11 @@ let stikerwelgc = "./galeria/welgc.webp"
 let stikerbyegc = "./galeria/byegc.webp"
 if (command == 'welcomegc') {
 conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ᴛʜᴇ 𝙳𝙾𝚁𝚁𝙰𝚃-𝙱𝙾𝚃-𝙼𝙳', body: '©𝙳𝚒𝚎𝚐𝚘𝙾𝚏𝚌', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})  
+contextInfo: { externalAdReply: { title: 'VIRUZZ-BOT-MD', body: 'VIRUZZ-OFC', sourceUrl: `https://github.com/VIRUZOFC/VIRUZZ-BOT-MD`, thumbnail: imagen1}}})  
 }
 if (command == 'byegc') {
 conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ᴛʜᴇ 𝙳𝙾𝚁𝚁𝙰𝚃-𝙱𝙾𝚃-𝙼𝙳', body: '©𝙳𝚒𝚎𝚐𝚘𝙾𝚏𝚌', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})
+contextInfo: { externalAdReply: { title: 'VIRUZZ-BOT-MD', body: 'VIRUZZ-OFC', sourceUrl: `https://github.com/VIRUZOFC/VIRUZZ-BOT-MD`, thumbnail: imagen1}}})
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
