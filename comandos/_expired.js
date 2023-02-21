b75d45ffe77d556ece4a610fa2aa6be2d8b31fbe
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-21 09:08:08 -0300

    Update _expired.js

--- comandos/_expired.js
+++ comandos/_expired.js
@@ -6,7 +6,7 @@ let chats = global.db.data.chats[m.chat]
 if (!chats.expired)
 return !0
 if (+new Date() > chats.expired) {
-let caption = `*EL BOT SE VA DEL GRUPO! SI QUIERE QUE VUELVA, USE EL COMANDO _#bottemporal_ PARA QUE VUELVA AL GRUPO!!*`
+let caption = `*Se acabo el tiempo asignado para la permanencia de el bot en este grupo, para hacer que vuelva use el comando #bottemporal*`
 let pp = './Menu2.jpg'
     
 await this.sendButton(m.chat, caption, wm, pp, [['𝙰𝙳𝙸𝙾𝚂', '.hastapronto']], null)
