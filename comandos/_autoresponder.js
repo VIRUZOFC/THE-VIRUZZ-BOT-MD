782c4141234cdf9ce2eb28c73e24675746b3a57f
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-21 08:56:38 -0300

    Update _autoresponder.js

--- comandos/_autoresponder.js
+++ comandos/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen4, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝘿𝙊𝙍𝙍𝘼𝙏-𝘽𝙊𝙏-𝙈𝘿', body: '𝑫𝑰𝑬𝑮𝑶-𝑶𝑭𝑪', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})}
+contextInfo: { externalAdReply: { title: 'VIRUZZ-BOT-MD', body: 'VIRUZZ-OFC', sourceUrl: `https://github.com/VIRUZOFC/VIRUZZ-BOT-MD`, thumbnail: imagen1}}})}
     
 return !0 }
 export default handler
