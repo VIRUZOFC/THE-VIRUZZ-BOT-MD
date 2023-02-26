77bbf0284c05dc21dd30d25306d9236688c9e9a7
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-26 10:15:10 -0300

    Update game-slot-info.js

--- comandos/game-slot-info.js
+++ comandos/game-slot-info.js
@@ -1,13 +1,13 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 let imagen = "https://telegra.ph/file/8904f74de010d9361cd61.jpg"
 let slot = `GAME - SLOT`
-let ejemplo = `*para jugar ablas apuestas debes escribir alguno de estos comandos*
+let ejemplo = `*para jugar a las apuestas debes escribir alguno de estos comandos*
 
-*slotlimit (se apuestan diamantes 5 minimo* )
+*#slotlimit (se apuestan diamantes 5 minimo* )*
 
-*slotxp (se apuesta experiencia 1000 minimo)*
+*#slotxp (se apuesta experiencia 1000 minimo)*
 
-*slotdolares (se apuestan dolares 10 minimo)*
+*#slotdolares (se apuestan dolares 10 minimo)*
 
 *mucha suerte a los jugadores*
 
