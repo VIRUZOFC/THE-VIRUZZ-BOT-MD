e1d4337dd6ae91661fd848ff211a8ba69b56928d
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-26 12:17:19 -0300

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -3,10 +3,10 @@ let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*\n
 *INFO: esta función es para usarse como una herramienta, no para andar jugando con ella, por esa razon tiene un precio alto.*`
 try {
-m.reply(`*⏰ Cargando, espere un momento*`)
+await m.reply(`*⏰ Cargando, espere un momento*`)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
-m.reply(`${hasil.result}`.trim())
+await m.reply(`${hasil.result}`.trim())
 } catch {
 throw `*[❕] ERROR, INTENTA DE NUEVO*`
 }}
