f53d483045e8cfe62cff4f9325257009b0c163e3
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-25 23:20:58 -0400

    Update game-slot-info.js

--- comandos/game-slot-info.js
+++ comandos/game-slot-info.js
@@ -1,5 +1,6 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 let imagen = "https://telegra.ph/file/8904f74de010d9361cd61.jpg"
+let slot = `GAME - SLOT`
 let ejemplo = `*para jugar ablas apuestas debes escribir alguno de estos comandos
 
 slotlimit (se apuestan diamantes 5 minimo )
@@ -10,7 +11,7 @@ slotdolares (se apuestan dolares 10 minimo)
 
 mucha suerte a los jugadores*`
 
-conn.sendButton(m.chat, ejemplo, imagen, wm, [['slot', `#slot`]], m)}
+conn.sendButton(m.chat, slot, ejemplo, imagen, [['slot', `#slot`]], m)}
 handler.help = ['ejemploslot']
 handler.command = ['slot']
 export default handler
