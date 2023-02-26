2cfbae6205fe002f4bb7efe42f5e8463189cecca
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-26 00:01:27 -0300

    Create game-slot-info.js

---
+++ comandos/game-slot-info.js
@@ -0,0 +1,16 @@
+let handler = async (m, { args, usedPrefix, command }) => {
+let imagen = `https://telegra.ph/file/8904f74de010d9361cd61.jpg`
+let ejemplo = `*para jugar ablas apuestas debes escribir alguno de estos comandos
+
+slotlimit (se apuestan diamantes 5 minimo )
+
+slotxp (se apuesta experiencia 1000 minimo)
+
+slotdolares (se apuestan dolares 10 minimo)
+
+mucha suerte a los jugadores*`
+
+coonsendButton(m.chat, xd, ejemplo, wm, [['slot', `#slot`]], m)}
+handler.help = ['ejemploslot']
+handler.command = ['slot']
+export default handler
