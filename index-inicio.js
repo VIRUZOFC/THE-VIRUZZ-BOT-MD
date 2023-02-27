dbdc402580efafa7a138e39679d46a32d3ce8dab
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-27 20:20:29 -0300

    Update index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('THE DORRAT-BOT-MD', {
+say('VIRUZZ-BOT MD', {
 font: 'chrome',
 align: 'center',
 colors: ['#ff8000']})
