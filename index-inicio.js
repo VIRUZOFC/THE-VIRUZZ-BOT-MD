9bea9e48cd8067477185b49fb20a2238679ac84b
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-27 20:53:37 -0300

    Update index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -14,7 +14,7 @@ const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
 say('VIRUZZ-BOT MD', {
-font: 'simpleBlock',
+font: 'simple',
 align: 'center',
 colors: ['green']})
 say(`CREADO POR DIEGO-OFC`, {
