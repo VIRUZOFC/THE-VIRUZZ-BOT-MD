978d60f206881484e017541a89b805b709245248
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-28 09:15:08 -0300

    Update index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -13,9 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('THE
-          VIRUZZ 
-                 BOT', {
+say('THE\nVIRUZZ\nBOT', {
 font: 'block',
 align: 'center',
 gradient: ['red', 'blue']})
