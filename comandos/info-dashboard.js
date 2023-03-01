fc7e4758e34e57b959ba089a2059adeb58f4ed7d
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-02-28 21:36:09 -0300

    Update info-dashboard.js

--- comandos/info-dashboard.js
+++ comandos/info-dashboard.js
@@ -7,9 +7,9 @@ let pp = imagen1
   })
   stats = stats.sort((a, b) => b.total - a.total)
   let txt = stats.slice(0, 15).map(({ name, total, last }, idx) => {
-    return `${htki} ${idx + 1} ${htka}
-𝐂𝐌𝐃\n⮕ ${name}
- 𝐇𝐈𝐓\n⮕ ${total}`
+    return `${htki} ${idx + 1} 
+${htjava} 𝐂𝐌𝐃 ${htjava} \n⮕ ${name}
+ ${htjava}  𝐇𝐈𝐓 ${htjava} \n⮕ ${total}`
 }).join`\n\n`
   m.reply(txt)}
 handler.help = ['dashboard']
