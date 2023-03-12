8219c9a39ddf4a43bf2424860276ae39ce6bd731
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-03-12 16:07:46 -0300

    Create mod-chetar.js

---
+++ comandos/mod-chetar.js
@@ -0,0 +1,25 @@
+import MessageType from '@adiwajshing/baileys'
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+        conn.reply(m.chat, `*éxito, ahora tiene todo ilimitado ✅*`, m)
+        global.db.data.users[m.sender].dolares = Infinity
+        global.db.data.users[m.sender].limit = Infinity
+        global.db.data.users[m.sender].level = Infinity
+        global.db.data.users[m.sender].bitcoins = Infinity
+}
+handler.help = ['cheat']
+handler.tags = ['owner']
+handler.command = /^(chetar|chetado)$/i
+handler.rowner = false
+handler.mods = true
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+//handler.money = 0
+
+export default handler
