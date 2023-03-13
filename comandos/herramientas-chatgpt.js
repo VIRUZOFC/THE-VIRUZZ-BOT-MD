30c5b00bb540cc5f186722b532e999b0aa94a777
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-03-12 22:19:00 -0300

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,16 +1,33 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*\n
-*INFO: esta función es para usarse como una herramienta, no para andar jugando con ella, por esa razon tiene un precio alto.*`
 try {
-await m.reply(`*⏰ Cargando, espere un momento*`)
-let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
-let hasil = await tiores.json()
-await m.reply(`${hasil.result}`.trim())
+if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*`
+await await m.reply(`*[ Cargando... ]*`)
+let tiores = await await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${zds}&text=${text}&user=user-unique-id`)
+let hasil = await await tiores.json()
+let openAi = `${hasil.result}`
+await await conn.sendMessage(m.chat, {
+     image: {
+         url: "https://cdn.discordapp.com/attachments/1084545752941998242/1084645752803958814/openai-chatgpt.jpg"
+     },
+     caption: openAi,
+     contextInfo: {
+         mentionedJid: [m.sender],
+         externalAdReply: {
+             title: `VIRUZZBOT - CHATGPT`,
+             sourceUrl: 'https://chat.whatsapp.com/L5oWIJp9c5BE8hMNCjJFGV',
+             mediaType: 1,
+             showAdAttribution: true,
+             thumbnailUrl: "https://cdn.discordapp.com/attachments/1084545752941998242/1084645752803958814/openai-chatgpt.jpg",
+             
+         }
+     }
+ }, {
+     quoted: m
+ })
 } catch {
-throw `*[❕] ERROR, INTENTA DE NUEVO*`
+conn.reply(m.chat,`*[❕] ERROR, INTENTA DE NUEVO*`)
 }}
 handler.command = ['openai', 'ia', 'robot']
-
-handler.dolares = 10
+handler.dolares = 4
 export default handler
