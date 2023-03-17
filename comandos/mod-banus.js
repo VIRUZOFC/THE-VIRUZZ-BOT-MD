112ffb829bba6f862f01d898310dce6c5bc1b66f
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-03-17 14:48:49 -0300

    Update mod-banus.js

--- comandos/mod-banus.js
+++ comandos/mod-banus.js
@@ -24,14 +24,14 @@ else who = m.chat
 let user = global.db.data.users[who]
 let name = conn.getName(who)
 let txt = text.replace(name).trim()
-if (!txt) return conn.reply(m.chat, `${mg}*Necesitas un motivo de baneo*\n` + `*${usedPrefix + command} @${who.split`@`[0]} *Motivo*`, fkontak, m)  
+if (!txt) return conn.reply(m.chat, `${mg}*Necesitas un motivo de baneo*\n` + `*${usedPrefix + command} @${who.split`@`[0]} Motivo*`, fkontak, m)  
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/9b1353deceded7f387713.jpg')
 let banu = `No deviste hacer eso papu`
-if (!who) throw `*ETIQUETA A LA PERSONA QUE QUIERA BANEAR Y AÑADA EL MOTIVO*`
+if (!who) throw `*ETIQUETA O COLOCA EL NUMERO DE LA PERSONA QUE QUIERA BANEAR Y AÑADA EL MOTIVO*`
 let chatstext = text.replace(who.split("@")[0], '').replace("@", '')
 let users = global.db.data.users
 users[who].banned = true
-await conn.sendButton(m.chat, `*✅ USUARIO BANEADO*\n*NOMBRE:* ${name}\n*NUMERO:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n*Razon: ${chatstext}*\n*Baneado por*: wa.me/${m.sender.split("@")[0]}`,  banu,  pp,[
+await conn.sendButton(m.chat, `*✅ USUARIO BANEADO*\n*NOMBRE:* ${name}\n*NUMERO:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n*Razon: ${chatstext}*\n*Baneado por*: wa.me/${m.sender.split("@")[0]}`,  banu, // pp,[
 ['Menú', `#menusimple`]], m)}
 handler.help = ['ban']
 handler.tags = ['mods']
